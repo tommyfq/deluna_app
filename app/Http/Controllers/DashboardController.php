500327c571589d91374c2d4d9b3cda6e04c06128
apeiharvey <hjoehanes@gmail.com>
2022-08-12 10:19:09 +0700

    dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
+
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\Stock;
 
 class DashboardController extends Controller {
     
@@ -19,6 +24,20 @@ class DashboardController extends Controller {
         $param['_title'] = 'Deluna | Dashboard';
         $param['_breadcrumbs'] = ['Dashboard' => route('dashboard.index')];
         $param['_sidebar'] = $this->menu;
+
+        $total_order = Order::sum('total_price');
+        $total_order_today = Order::where('created_at', '>=', date('Y-m-d'))->sum('total_price');
+        $total_product = Product::count('id');
+        $group_stock = Product::leftJoin(with(new Stock)->getTable().' as s', function($join){
+                                    $join->on('s.product_id', with(new Product)->getTable().'.id');
+                                })
+                                ->groupBy(with(new Product)->getTable().'.id', with(new Product)->getTable().'.name')
+                                ->get([with(new Product)->getTable().'.id', with(new Product)->getTable().'.name', DB::raw('sum(s.stock) as total_stock')]);
+
+        $param['total_order'] = $total_order;
+        $param['total_order_today'] = $total_order_today;
+        $param['total_product'] = $total_product;
+        $param['group_stock'] = $group_stock;
         
         $viewtarget = "pages.dashboard.index";
         $content = view($viewtarget, $param);
--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -5,6 +5,40 @@
                 <div class="card-body">
                     @include('_includes.alert')
                     <div class="row">
+                        <div class="col-lg-3 col-sm-6">
+                            <div class="card gradient-2">
+                                <div class="card-body">
+                                    <h3 class="card-title text-white">Total Order</h3>
+                                    <div class="d-inline-block">
+                                        <h2 class="text-white">Rp. {{$total_order ? number_format($total_order,2,",",".") : '0'}}</h2>
+                                    </div>
+                                    <span class="float-right display-5 opacity-5"><i class="fa fa-money"></i></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-sm-6">
+                            <div class="card gradient-3">
+                                <div class="card-body">
+                                    <h3 class="card-title text-white">Total Order Today</h3>
+                                    <div class="d-inline-block">
+                                        <h2 class="text-white">Rp. {{$total_order_today ? number_format($total_order_today,2,",",".") : '0'}}</h2>
+                                        <p class="text-white mb-0">{{date('Y-m-d')}}</p>
+                                    </div>
+                                    <span class="float-right display-5 opacity-5"><i class="fa fa-money"></i></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-sm-6">
+                            <div class="card gradient-1">
+                                <div class="card-body">
+                                    <h3 class="card-title text-white">Products Available</h3>
+                                    <div class="d-inline-block">
+                                        <h2 class="text-white">{{$total_product}}</h2>
+                                    </div>
+                                    <span class="float-right display-5 opacity-5"><i class="fa fa-shopping-cart"></i></span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
